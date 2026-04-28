f1f9f53a3b548ff27d0b9e69c34bcd12e323f920
Sujal Kesarkar <sujalkesarkar0@gmail.com>
2026-04-28 13:30:51 +0530

    Add Dark mode funcitinality and fixing some styling.

--- public/js/script.js
+++ public/js/script.js
@@ -17,7 +17,23 @@
 
                 form.classList.add("was-validated");
             },
-            false
+            false,
         );
     });
 })();
+
+const html = document.documentElement;
+const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme") || "light";
+html.setAttribute("data-bs-theme", savedTheme);
+
+// Toggle theme
+toggleBtn.addEventListener("click", () => {
+    const currentTheme = html.getAttribute("data-bs-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    html.setAttribute("data-bs-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+});
